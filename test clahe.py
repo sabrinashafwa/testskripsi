df7dd2dc71443c5d7abd98b020d63aa7627bb725
sabrinashafwa <83755298+sabrinashafwa@users.noreply.github.com>
2025-02-12 12:55:55 +0700

    Update test clahe.py

--- test clahe.py
+++ test clahe.py
@@ -1,19 +1,17 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
-from PIL import Image
 import cv2
+from PIL import Image
 
-def clahe_enhance(image_path, clip_limit=2.0, tile_grid_size=(8, 8)):
-   
+def clahe_enhance(image_array, clip_limit=2.0, tile_grid_size=(8, 8)):
     try:
-        img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Read as grayscale
-        if img is None:
-            print(f"Error: Could not load image at {image_path}")
-            return None
+        # Konversi ke grayscale
+        img_gray = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
 
+        # Terapkan CLAHE
         clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
-        enhanced_img = clahe.apply(img)
+        enhanced_img = clahe.apply(img_gray)
+
         return enhanced_img
 
     except Exception as e:
@@ -22,19 +20,24 @@ def clahe_enhance(image_path, clip_limit=2.0, tile_grid_size=(8, 8)):
 
 # Streamlit app
 def main():
-    st.title("coba clahe")
-    st.write("preprop.")
+    st.title("Coba CLAHE")
+    st.write("Preprocessing Gambar Fundus dengan CLAHE")
 
     uploaded_file = st.file_uploader("Unggah gambar fundus", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        # Baca gambar yang diunggah
-        image = Image.open(uploaded_file).convert("RGB")  # Pastikan gambar selalu RGB
+        # Baca gambar dan konversi ke RGB
+        image = Image.open(uploaded_file).convert("RGB")
         st.image(image, caption="Gambar yang diunggah", use_column_width=True)
-        
-        # Preprocessing gambar
+
+        # Konversi ke numpy array
         image_array = np.array(image)
+        
+        # Terapkan CLAHE
         preprocessed_image = clahe_enhance(image_array)
+
+        if preprocessed_image is not None:
+            st.image(preprocessed_image, caption="Hasil CLAHE", use_column_width=True, clamp=True)
 
 if __name__ == "__main__":
     main()
